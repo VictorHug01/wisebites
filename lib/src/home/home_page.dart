298cca9228faffb7d783734fcde3c8df8b682841
Victor <victorhugobbpereira02@gmail.com>
2023-11-24 23:12:49 -0300

    fazendo responsividade

--- lib/src/home/home_page.dart
+++ lib/src/home/home_page.dart
@@ -13,26 +13,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int screenWidth = 0;
+
+  Visu() {
+    if (MediaQuery.of(context).size.width < 650) {
+      return screenWidth = 1;
+    } else {
+      return screenWidth = 2;
+    }
+  }
+
   List<Widget> imagens = [
     SizedBox(
       width: double.infinity,
       child: Image.asset(
         'assets/s1.png',
-        fit: BoxFit.fitWidth,
+        fit: BoxFit.cover,
       ),
     ),
     SizedBox(
       width: double.infinity,
       child: Image.asset(
         'assets/s2.png',
-        fit: BoxFit.fitWidth,
+        fit: BoxFit.cover,
       ),
     ),
     SizedBox(
       width: double.infinity,
       child: Image.asset(
         'assets/s3.png',
-        fit: BoxFit.fitWidth,
+        fit: BoxFit.cover,
       ),
     ),
   ];
@@ -40,109 +50,120 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       endDrawer: const DrawerComponent(),
-      appBar: AppBar(
-        toolbarHeight: 80.0,
-        leadingWidth: 150.0,
-        foregroundColor: Color(0XFFFF0000),
-        surfaceTintColor: Colors.transparent,
-        leading: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-          child: Image.asset(
-            'assets/logo.png',
-            fit: BoxFit.contain,
-          ),
-        ),
-        actions: [
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-            child: MediaQuery.of(context).size.width < 650
-                ? Builder(builder: (context) {
-                    return IconButton(
-                      onPressed: () {
-                        Scaffold.of(context).openEndDrawer();
-                      },
-                      icon: const Icon(Icons.menu),
-                    );
-                  })
-                : const Row(
-                    children: [
-                      ListSpace(textList: 'Home'),
-                      ListSpace(textList: 'Sobre nós'),
-                      ListSpace(textList: 'Como funciona'),
-                      ListSpace(textList: 'Custos'),
-                      ListSpace(textList: 'Escolas'),
-                    ],
-                  ),
-          ),
-        ],
-      ),
-      body: ListView(
-        children: [
-          SizedBox(
-            height: 8.0,
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: CarouselSlider(
-              options: CarouselOptions(
-                autoPlayCurve: Curves.easeInOutSine,
-                viewportFraction: 1.0,
-                height: 350,
-                autoPlay: true,
-                autoPlayInterval: const Duration(seconds: 3),
-              ),
-              items: imagens.map((Image) => Image).toList(),
-            ),
-          ),
-          Container(
-            height: 350.0,
-            child: Padding(
+      body: NestedScrollView(
+        floatHeaderSlivers: true,
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          SliverAppBar(
+            toolbarHeight: 80.0,
+            leadingWidth: 150.0,
+            foregroundColor: Color(0XFFFF0000),
+            surfaceTintColor: Colors.transparent,
+            leading: Padding(
               padding:
                   const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      alignment: Alignment.centerLeft,
-                      child: Image.asset(
-                        'assets/logo.png',
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Porque WiseBites?',
-                            style: GoogleFonts.montserrat(
-                              fontWeight: FontWeight.w400,
-                              fontSize: 60.0,
-                              color: Color(0XFFFF0000),
-                            ),
-                          ),
-                          Text(
-                            'DESCULBRA OS BENEFÍCIOS DA NOSSA \n INICIATIVA.',
-                            style: GoogleFonts.montserrat(
-                              fontWeight: FontWeight.w400,
-                              fontSize: 20.0,
-                              color: Color(0XFFFF0000),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
+              child: Image.asset(
+                'assets/logo.png',
+                fit: BoxFit.contain,
               ),
             ),
+            actions: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+                child: MediaQuery.of(context).size.width < 650
+                    ? Builder(builder: (context) {
+                        return IconButton(
+                          onPressed: () {
+                            Scaffold.of(context).openEndDrawer();
+                          },
+                          icon: const Icon(Icons.menu),
+                        );
+                      })
+                    : Row(
+                        children: [
+                          ListSpace(textList: 'Home'),
+                          ListSpace(textList: 'Sobre nós'),
+                          ListSpace(textList: 'Como funciona'),
+                          ListSpace(textList: 'Custos'),
+                          ListSpace(textList: 'Escolas'),
+                        ],
+                      ),
+              ),
+            ],
           ),
         ],
+        body: ListView(
+          children: [
+            SizedBox(
+              height: 8.0,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  autoPlayCurve: Curves.easeInOutSine,
+                  viewportFraction: 1.0,
+                  height:
+                      MediaQuery.of(context).size.width < 650 ? 200.0 : 300.0,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 3),
+                ),
+                items: imagens.map((Image) => Image).toList(),
+              ),
+            ),
+            GridView.count(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+              crossAxisCount: Visu(),
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              childAspectRatio:
+                  MediaQuery.of(context).size.width >= 650 ? 16 / 7 : 32 / 9,
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: Image.asset(
+                    'assets/logo.png',
+                    width: MediaQuery.of(context).size.width / 3.0,
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: MediaQuery.of(context).size.width < 650
+                        ? CrossAxisAlignment.end
+                        : CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Porque WiseBites?',
+                          softWrap: false,
+                          style: GoogleFonts.montserrat(
+                            fontWeight: FontWeight.w400,
+                            fontSize: MediaQuery.of(context).size.width / 20.0,
+                            color: Color(0XFFFF0000),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'DESCULBRA OS BENEFÍCIOS DA NOSSA \n INICIATIVA.',
+                          softWrap: false,
+                          style: GoogleFonts.montserrat(
+                            fontWeight: FontWeight.w400,
+                            fontSize: MediaQuery.of(context).size.width / 45.0,
+                            color: Color(0XFFFF0000),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -155,11 +176,15 @@ class ListSpace extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(right: 10.0),
-      child: Text(
-        textList,
-        style: GoogleFonts.montserrat(
-          fontWeight: FontWeight.w500,
+      padding: const EdgeInsets.only(right: 15.0),
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: Text(
+          textList,
+          style: GoogleFonts.montserrat(
+            fontWeight: FontWeight.w500,
+            fontSize: 15.0,
+          ),
         ),
       ),
     );
